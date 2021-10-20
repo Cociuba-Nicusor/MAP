9091bdd6a314b5fea1056dbf7adfd674379e44df
Cociuba-Nicusor <cociubanicusor66@gmail.com>
2021-10-20 22:57:51 +0300

    ty

---
+++ Coada/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Coada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue A = new Queue();
+            A.Push(1);
+            A.Push(2);
+            A.Push(3);
+            A.Push(4);
+            A.Push(5);
+            A.Push(6);
+            A.Pop();
+            A.view();
+            Console.ReadKey();
+        }
+    }
+}
